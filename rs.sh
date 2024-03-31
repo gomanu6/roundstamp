d220c2672d6eece98f3343f6932bcabe932defb4
Manu <gomanu6@gmail.com>
2024-03-31 16:32:27 +0530

    Adding changes

--- rs.sh
+++ rs.sh
@@ -13,6 +13,10 @@ todays_date=$(date +%F)
 
 . ./rs.config
 
+if [ "$#" -le 1 ]; then
+    echo "${p} File not specified"
+    exit 1
+fi
 
 echo "${p} Checking if file exists"
 if [ -f "$1" ]; then

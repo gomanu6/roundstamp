4d235be1a40f82a3b9d8bb6da8153aaa91c74eaa
Manu <gomanu6@gmail.com>
2024-04-27 21:56:30 +0530

    trying pixmap manipulations

--- helpers/create_pixmaps.py
+++ helpers/create_pixmaps.py
@@ -3,10 +3,11 @@
 import os
 from pathlib import Path
 import fitz
+from helpers.dpprint import dpprint
 
 
 
-def create_pixmaps(list, src_dir, dst_dir):
+def create_pixmaps(list, src_dir, dst_dir, stamp_file):
     final_list = []
 
     for item in list:
@@ -18,7 +19,7 @@ def create_pixmaps(list, src_dir, dst_dir):
     for file in final_list:
         src_file = file[0]
         dst_file = file[1]
-
+        print("Processing ", src_file)
         doc = fitz.open(src_file)
         matrix = fitz.Matrix(1, 1)    
 
@@ -32,8 +33,42 @@ def create_pixmaps(list, src_dir, dst_dir):
             img_path = os.path.join(dst_file, img_filename)
             pix.save(img_path)
         
-        for img in os.listdir(dst_file):
-            print(dst_file, img)
+        img_list = os.listdir(dst_file)
+        img_list.sort()
+        # dpprint(img_list)
+
+        for i, f in enumerate(img_list):
+            print("i is -->", i)
+            print("f is -->", f)
+        # new_doc = fitz.open()
+        # for img in os.listdir(dst_file):
+        #     print(dst_file, img)
+        #     imgpath = os.path.join(dst_file, img)
+        #     print(imgpath)
+        #     # st_pg = img.split(".")[0]
+        #     # st_pagename = "st_%s.pdf" % (st_pg)
+        #     imgfile = fitz.open(imgpath)
+        #     rect = imgfile[0].rect
+        #     pdfbytes = imgfile.convert_to_pdf()
+        #     imgfile.close
+
+        #     imgpdf = fitz.open("pdf", pdfbytes)
+
+        #     page = new_doc.new_page(width = rect.width, height = rect.height)
+        #     page.show_pdf_page(rect, imgpdf, 0)
+        # new_doc.ez_save()
+
+
+
+            # print(type(doc))
+            # page = doc[0]
+            # print(type(page))
+            # start_width = page.rect.width - 100
+            # start_height = page.rect.height - 100
+            # coords = fitz.Rect(start_width, start_height, start_width + 75, start_height + 75)
+            # page.insert_image(coords, filename=stamp_file)
+            # save_path = os.path.join(dst_file, st_pagename)
+            # doc.ez_save(save_path)
             # pix = fitz.Pixmap(img)
             # imgpdf = fitz.open("pdf", pix.pdfocr_tobytes())
             # doc.insert_pdf(imgpdf)

2d9de595952ce195ec8cd921900fef7586d368a8
Manu <gomanu6@gmail.com>
2024-04-29 21:36:05 +0530

    testing conversion to pdf

--- helpers/stamp_pdf.py
+++ helpers/stamp_pdf.py
@@ -6,10 +6,35 @@ import sys
 from pathlib import Path
 import fitz
 
+stamp_width = 75
+stamp_height = 75
 
+dist_right = 100
+dist_bottom = 100
 
-def stamp_pdf_page(page):
-    print(page)
+def stamp_pdf_page(dir, page, stamp, stamp_width, stamp_height, dist_right, dist_bottom):
+
+    open_path = os.path.join(dir, page)
+    doc = fitz.open(open_path)
+    new_name = "st_" + str(page)
+    save_path = os.path.join(dir, new_name)
+
+    for page in doc:
+        page_width = page.rect.width
+        page_height = page.rect.height
+
+        start_width = (page_width - dist_right)
+        start_height = (page_height - dist_bottom)
+
+        end_width = start_width + stamp_width
+        end_height = start_height + stamp_height
+    
+        coords = fitz.Rect(start_width, start_height, end_width, end_height)
+
+        page.insert_image(coords, filename=stamp)
+    
+    doc.ez_save(save_path)
+
 
 
 

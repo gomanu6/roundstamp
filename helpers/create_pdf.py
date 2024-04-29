25a37308cf12f4503d8c2675c6aa8354b680e617
Manu <gomanu6@gmail.com>
2024-04-29 21:23:27 +0530

    testing conversion to pdf

--- helpers/create_pdf.py
+++ helpers/create_pdf.py
@@ -9,19 +9,26 @@ from helpers.dpprint import dpprint
 
 def create_pdf_from_images(list):
     print("Welcome to Create PDF from Images")
+
+    # pdfs_created = {}
+
+
+
     for dir in list:
         print("Processing ... ", dir)
         # print(os.path.isdir(file))
         # print(file)
+        file_name = os.path.basename(dir)
+        # pdfs_created[file_name] = []
 
         doc = fitz.open()
-        save_path = os.path.join(dir, os.path.basename(dir))
+        save_path = os.path.join(dir, file_name)
         list_of_images = os.listdir(dir)
         list_of_images.sort()
         # dpprint(list_of_images)
 
         for i, f in enumerate(list_of_images):
-
+            # pdfs_created[file_name].append(f)
             img = fitz.open(os.path.join(dir, f))
             rect = img[0].rect
             pdfbytes = img.convert_to_pdf()
@@ -32,6 +39,8 @@ def create_pdf_from_images(list):
             page.show_pdf_page(rect, imgPDF, 0)
 
         doc.save(save_path)
+    
+    # return pdfs_created
 
 
 
